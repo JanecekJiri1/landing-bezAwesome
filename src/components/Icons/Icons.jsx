34c11cf6de8a2c0faddfae85342b6eb5295ee04e
Jirka1993 <kleric13@seznam.cz>
2022-01-09 13:03:19 +0100

    bez font awesome

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -1,43 +1,35 @@
 import classes from "./Icons.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faCoffee } from "@fortawesome/free-solid-svg-icons"; /* Připomínka pro mě */
-// import { faGooglePlusSquare } from "@fortawesome/free-brands-svg-icons";
-import {
-  faGooglePlusG,
-  faFacebookF,
-  faTwitter,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// // import { faCoffee } from "@fortawesome/free-solid-svg-icons"; /* Připomínka pro mě */
+// // import { faGooglePlusSquare } from "@fortawesome/free-brands-svg-icons";
+// import {
+//   faGooglePlusG,
+//   faFacebookF,
+//   faTwitter,
+//   faInstagram,
+// } from "@fortawesome/free-brands-svg-icons";
 
 const Icons = (props) => {
   return (
     <div className={classes.icons}>
       <div className={classes.icon}>
         <a href="https://www.google.cz/">
-          <div className={classes.google}>
-            <FontAwesomeIcon icon={faGooglePlusG} />
-          </div>
+          <div className={classes.google}>{/* <FontAwesomeIcon icon={faGooglePlusG} /> */}</div>
         </a>
       </div>
       <div className={classes.icon}>
         <a href="https://www.facebook.com/">
-          <div className={classes.facebook}>
-            <FontAwesomeIcon icon={faFacebookF} />
-          </div>
+          <div className={classes.facebook}>{/* <FontAwesomeIcon icon={faFacebookF} /> */}</div>
         </a>
       </div>
       <div className={classes.icon}>
         <a href="https://twitter.com/">
-          <div className={classes.twitter}>
-            <FontAwesomeIcon icon={faTwitter} />
-          </div>
+          <div className={classes.twitter}>{/* <FontAwesomeIcon icon={faTwitter} /> */}</div>
         </a>
       </div>
       <div className={classes.icon}>
         <a href="https://www.instagram.com/">
-          <div className={classes.instagram}>
-            <FontAwesomeIcon icon={faInstagram} />
-          </div>
+          <div className={classes.instagram}>{/* <FontAwesomeIcon icon={faInstagram} /> */}</div>
         </a>
       </div>
     </div>
